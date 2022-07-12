7346d3b20cce46941cfb9b963b0491b185fdf04b
Anton Orekhovskiy <anton.o.0619@gmail.com>
2022-07-12 23:43:50 +0300

    feat: add title filter

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-import createError from 'http-errors';
 import express from 'express';
-import path from 'path';
 
-import * as  issuesRouter from './routes/issues.js'
-
+import * as  issuesRouter from './routes/issues.js';
 import { getIssues } from './routes/issues.service.js';
 
 const issuesService = () => {
@@ -25,7 +22,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/issues', exposeIssuesService, issuesRouter.router);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.status(404).json({ error: 'Not found' });
   return;
--- routes/issues.service.js
+++ routes/issues.service.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-async function getIssues(owner, repo, queryParams) {
-    console.log(`Fetching data from repository ${repo} owned by ${owner}`)
-    console.log(`Fetching with folowing query params: \n ${JSON.stringify(queryParams)}`)
+async function getIssues(owner, repo, params) {
+    console.log(`Fetching data from repository ${repo} owned by ${owner}`);
+    console.log(`Fetching with folowing query params: \n ${JSON.stringify(params)}`);
     const url = `https://api.github.com/repos/${owner}/${repo}/issues`;
-    const results = await axios.get(url, { params: queryParams });
-    return results.data;
+    let results = (await axios.get(url, { params })).data;
+    if (params.title) {
+        results = results.filter(item => 
+            item.title === params.title
+        );
+    }
+    return results;
 }
 
 export { getIssues };
